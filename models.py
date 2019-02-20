d7c1f9b5d745c1443c224a176a30471a5b5dabcc
annika <annikaslund@gmail.com>
2019-02-20 13:46:52 -0800

    Add user model

--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """ Connect to database. """
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """ User. """ 
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    first_name = db.Column(db.String(30),
+                    nullable=False),
+    last_name = db.Column(db.String(30),
+                    nullable=False),
+    image_url = db.Column(db.String(200),
+                    nullable=True,
+                    default="https://www.top13.net/wp-content/uploads/2015/10/perfectly-timed-cat-photos-funny-cover.jpg")
